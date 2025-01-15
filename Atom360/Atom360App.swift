fd58168d343c62a7962841c9f4fe7a50e21d51a0
AniketKumar090 <kumaraniket009@gmail.com>
2025-01-15 18:26:33 +0530

    Initial Commit

---
+++ Atom360/Atom360App.swift
@@ -0,0 +1,17 @@
+//
+//  Atom360App.swift
+//  Atom360
+//
+//  Created by Aniket Kumar on 15/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Atom360App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Atom360/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Atom360
+//
+//  Created by Aniket Kumar on 15/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
