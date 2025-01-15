dc1d3b1edcde07b7fc4bb74c101ea5a1c9625207
AniketKumar090 <kumaraniket009@gmail.com>
2025-01-15 23:25:20 +0530

    Task Completed

--- Atom360/ContentView.swift
+++ Atom360/ContentView.swift
@@ -1,24 +1,9 @@
-//
-//  ContentView.swift
-//  Atom360
-//
-//  Created by Aniket Kumar on 15/01/25.
-//
-
 import SwiftUI
+import SimpleCameraLibrary
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+        CameraView()
     }
-}
-
-#Preview {
-    ContentView()
 }
